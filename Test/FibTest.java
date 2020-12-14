7dd1a044aec32a979ee8ead4e6b6ad5c45ad29fa
Bradley Vaal <bavaal@bsu.edu>
2020-12-14 15:37:52 -0500

    actual working fib calc

--- Test/FibTest.java
+++ Test/FibTest.java
@@ -1,14 +1,47 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class FibTest {
+    FibonacciCalculator fib = new FibonacciCalculator();
+    Scanner input = new Scanner(System.in);
     @Test
     public void TestBaseCaseIsOne(){
-        FibonacciCalculator fib = new FibonacciCalculator();
-        fib.makeSequence(0,0);
-        int baseCase = fib.getValueAt(0);
+        fib.makeSequence(0,1);
+        int baseCase = fib.getValueAt(1);
         Assertions.assertEquals(1,baseCase);
     }
+    @Test
+    public void TestCaseTwoIsOne(){
+        fib.makeSequence(0,1);
+        int secondCase = fib.getValueAt(1);
+        Assertions.assertEquals(1, secondCase);
+    }
+    @Test
+    public void TestCaseThreeIsTwo(){
+        fib.makeSequence(0,3);
+        int thirdCase = fib.getValueAt(3);
+        System.out.println(fib.fibSequence);
+        Assertions.assertEquals(2,thirdCase);
+    }
+    @Test
+    public void TestCaseFourIsThree(){
+        fib.makeSequence(0,4);
+        int fourthcase = fib.getValueAt(4);
+        System.out.println(fib.fibSequence);
+        Assertions.assertEquals(3, fourthcase);
+    }
+    @Test
+    public void TestArrayToTen(){
+        fib.makeSequence(0,10);
+        int fourthcase = fib.getValueAt(10);
+        ArrayList<Integer> calculatedSeq = fib.fibSequence;
+        System.out.println(calculatedSeq);
+        Assertions.assertEquals(fib.fibSequence.size(),10);
+    }
+
 
 
 
--- src/FibonacciCalculator.java
+++ src/FibonacciCalculator.java
@@ -2,13 +2,34 @@ import java.util.ArrayList;
 
 public class FibonacciCalculator {
 
+
     ArrayList<Integer> fibSequence = new ArrayList();
-    public void makeSequence(int i, int i1) {
-        fibSequence.add(1);
+
+    public FibonacciCalculator() {
+
+    }
+
+    public void makeSequence(int i, int end) {
+
+        for(int j = 0; j<end; j++){
+            fibSequence.add(Fibonacci(j));
+        }
     }
 
     public int getValueAt(int i) {
-        int value = fibSequence.get(i);
+        int value = fibSequence.get(i-1);
         return value;
     }
+    public int Fibonacci(int counter){
+        if(counter<= 1){
+            return 1;
+            }
+        return Fibonacci(counter-1)+ Fibonacci(counter-2);
+    }
+
+
+    public void getSequence(int counter) {
+
 }
+}
+
